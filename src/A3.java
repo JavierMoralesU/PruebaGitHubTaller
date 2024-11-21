9ceb5d97ad192871092973e572f9c37e34e80d38
JavierMoralesU <a2193273017@alumnos.uat.edu.mx>
2024-11-21 17:31:41 -0600

    C3

---
+++ src/A3.java
@@ -0,0 +1,10 @@
+public class A3 {
+        
+        public static void main(String[] args) {
+            System.out.println("Ejercicio de multiplicación");
+            int n1 = 1;
+            int n2 = 2;
+            int mult = n1 * n2;
+            System.out.println("La multiplicación de " + n1 + " y " + n2 + " es " + mult);
+        }
+}
