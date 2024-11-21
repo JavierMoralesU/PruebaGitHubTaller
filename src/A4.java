e6d08c8b05027c3a51704df5d327abcbcfd44549
JavierMoralesU <a2193273017@alumnos.uat.edu.mx>
2024-11-21 17:58:43 -0600

    C4

---
+++ src/A4.java
@@ -0,0 +1,10 @@
+public class A4 {
+    
+    public static void main(String[] args) {
+        System.out.println("Ejercicio de división");
+        int n1 = 1;
+        int n2 = 2;
+        int resultado = n1 / n2;
+        System.out.println("El resultado de la división es: " + resultado);
+    }
+}
