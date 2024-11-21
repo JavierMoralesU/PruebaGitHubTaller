8bb14d93c4be07ecdc1c918071fe78d0cf7c6687
JavierMoralesU <a2193273017@alumnos.uat.edu.mx>
2024-11-21 17:18:20 -0600

    c1

---
+++ src/A2.java
@@ -0,0 +1,9 @@
+public class A2 {
+    
+    public static void main(String[] args) {
+        int n1 = 1;
+        int n2 = 2;
+        int sum = n1 + n2;
+        System.out.println("Sum of " + n1 + " and " + n2 + " is " + sum);
+    }
+}
