986fc04ce6fa0f451df1962a78d48a685a1c8ce8
JavierMoralesU <a2193273017@alumnos.uat.edu.mx>
2024-11-21 18:04:05 -0600

    C5.1

--- src/A5.java
+++ src/A5.java
@@ -1,9 +1,9 @@
 public class A5 {
     public static void main(String[] args) {
         
-        System.out.println("Ejercicio de cuadrado");
+        System.out.println("Ejercicio de cubo");
         int n1 = 2;
-        int resultado = n1 * n1;
-        System.out.println("El resultado del cuadrado es: " + resultado);
+        int resultado = n1 * n1 * n1;
+        System.out.println("El resultado del cubo es: " + resultado);
     }
 }
