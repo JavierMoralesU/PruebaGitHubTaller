777e6c7b5c003c620d2404bab37fe804bea77f1d
JavierMoralesU <a2193273017@alumnos.uat.edu.mx>
2024-11-21 18:02:57 -0600

    C5

---
+++ src/A5.java
@@ -0,0 +1,9 @@
+public class A5 {
+    public static void main(String[] args) {
+        
+        System.out.println("Ejercicio de cuadrado");
+        int n1 = 2;
+        int resultado = n1 * n1;
+        System.out.println("El resultado del cuadrado es: " + resultado);
+    }
+}
