ce81ee18e71872a1ca3861668fbb35e8f148b053
JavierMoralesU <a2193273017@alumnos.uat.edu.mx>
2024-11-23 20:18:14 -0600

    CR2

---
+++ src/R2.java
@@ -0,0 +1,21 @@
+public class R2 {
+    
+    public static void main(String[] args) {
+        
+        int n = 1;
+
+        for (n=1 ; n <= 10; n++) {
+            
+            System.out.println("cantidad: "+n);
+            for(int i = 1; i <= n; i++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+
+    }
+
+
+}
